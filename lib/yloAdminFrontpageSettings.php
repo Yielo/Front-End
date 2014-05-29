7b80197793e2fc43e054aacc05987b7ba84f00a0
audaxland <github2014@audaxland.fr>
2014-05-29 11:01:06 +0200

    création d'un select pour le lien final
    permet de désigner le lien final de la front page à partir d'un menu déroulant connant la liste des pages existantes

--- lib/yloAdminFrontpageSettings.php
+++ lib/yloAdminFrontpageSettings.php
@@ -3,8 +3,7 @@ yieloCenter::includeClassFile('yloFrontpageSettings');
 
 class yloAdminFrontpageSettings extends yloFrontpageSettings
 {
-	
-	
+
 	public function init(){
 
 		// cette méthode est l'endroit où placer les crochets relatif à l'objet
@@ -84,7 +83,8 @@ class yloAdminFrontpageSettings extends yloFrontpageSettings
 				
 				<h3>Le lien final</h3>
 				Lien : 
-				<input type="text" name="yielo_front_textes[front3_lien]" value="<?php $this->front3_lien(); ?>" /><br />
+				<?php $this->display_page_select(true);?>
+				<br />
 				
 				<hr>
 				<input type="submit" value="<?php echo esc_attr_e('Update Options'); ?>" class="button-primary" />
@@ -94,5 +94,60 @@ class yloAdminFrontpageSettings extends yloFrontpageSettings
 			<?php 		
 	}
 	
-
+	protected function get_les_pages(){
+		// renvoie la liste des pages existates en séparant les pages d'inscription des autres pages
+		$pages_d_inscription = array();
+		$autres_pages_publiee = array();
+		$autres_pages_non_publiee = array();
+		$id_url = array();
+		$args = array(
+				'sort_order' => 'ASC',
+				'sort_column' => 'ID',
+				'post_type' => 'page',
+				'meta_key'     => '_wp_page_template',
+		);
+		$pages = get_pages($args);
+		foreach($pages as $page){
+			$item = new stdClass();
+			$item->ID 	= $page->ID;
+			$item->post_title 	= $page->post_title;
+			$item->post_name	=	$page->post_name;
+			$item->post_status	=	$page->post_status;
+			$item->guid			=	$page->guid;
+			$item->template		=	$page->meta_value;
+			$id_url[$page->ID]	=	$page->guid;
+			if($item->template == 'page-inscription.php' && $item->post_status = 'publish'){
+				$pages_d_inscription[] = $item;
+			}elseif($item->template == 'page-inscription.php' && !($item->post_status = 'publish')){
+				array_unshift($autres_pages_non_publiee, $item);
+			}elseif($item->post_status = 'publish'){
+				$autres_pages_publiee[] = $item;
+			}else{
+				$autres_pages_non_publiee[] = $item;
+			}
+		}
+		$retour = new stdClass();
+		$retour->inscription = $pages_d_inscription;
+		$retour->autre	= $autres_pages_publiee;
+		$retour->non_publiee = $autres_pages_non_publiee;
+		$retour->liste = $id_url;
+		return $retour;
+	}
+	
+	protected function display_page_select($echo = true){
+		$pages = $this->get_les_pages();
+		$old_url = $this->FrontTextes['front3_lien'];
+		$str = '<select name="yielo_front_textes[front3_lien]">';
+		foreach($pages->inscription as $page)
+			$str .= '<option value="'.esc_attr($page->guid).'" '.selected($old_url, $page->guid, false).'>'.esc_attr($page->post_title).' (page d&#39;inscription)</option>';
+		$str .= '<option disabled>--------------</option>';
+		foreach($pages->autre as $page)
+			$str .= '<option value="'.esc_attr($page->guid).'" '.selected($old_url, $page->guid, false).'>'.esc_attr($page->post_title).' (autre page)</option>';
+		$str .= '<option disabled>--------------</option>';
+		foreach($pages->non_publiee as $page)
+			$str .= '<option value="'.esc_attr($page->guid).'" disabled>'.esc_attr($page->post_title).' (non publi&eacute;e)</option>';
+		$str .= '</select>';
+		if($echo) echo $str;
+		return $str;
+	}
 }
