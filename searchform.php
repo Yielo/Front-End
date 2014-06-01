e52dad2a3df4d003d648eed99363112a696ce01f
audaxland <github2014@audaxland.fr>
2014-06-01 18:41:08 +0200

    Obsolet donc supprimé

--- searchform.php
+++
@@ -1,8 +0,0 @@
-	<div class="ylo-search-container">
-		<form id="search" method="get" action="<?php echo home_url( '/' ); ?>" >
-			<fieldset>
-				<input type="text" name="s" value="" placeholder="Un membre, un métier , un lieu,..." />
-				<input type="submit" value="Rechercher" class="ylo-search-button" />
-			</fieldset>
-		</form>
-	</div>
