c870e0853c924876d431d3c5e61374b1e9b67a28
audaxland <github2014@audaxland.fr>
2014-06-01 18:38:56 +0200

    Update single.php

--- single.php
+++ single.php
@@ -7,24 +7,33 @@
 			<h1 class="ylo-main-title"><?php the_title(); ?></h1>
 			<div id="ylo-main">
 				<div class="ylo-col1">
-					<?php while(have_posts()) : the_post();?>
-						<div <?php post_class('ylo-post-article'); ?>>
-							<h2 class="ylo-titre-triangle">Publi&eacute; le <?php echo get_the_date();?>, par <?php the_author(); ?><span>&nbsp;</span></h2>
-							<p><?php the_content(); ?></p>		
-						</div>
-						<nav class="ylo-single-page-nav">
-							<?php posts_nav_link(); ?>
-						</nav>
-						<?php comments_template();?>
-					<?php endwhile;?>
-					&nbsp;
-					<?php if(current_user_can('edit_posts')) :?>
-						<hr />
-						Publier un nouvel article dans cette la cat&eacute;gorie  
-						<a href="<?php echo (home_url('/wp-admin/post-new.php')); ?>" >
-							<button>Nouvel Article</button>
-						</a>
-						<hr><br>
+					<?php if(isset($_GET['ylo_editor']) && $_GET['ylo_editor'] == 'edit' && current_user_can('edit_posts')) : ?>
+						<?php do_action('ylo_edit_post_editor');?>
+					<?php elseif(isset($_GET['ylo_delete']) && $_GET['ylo_delete'] == 'current_post') :?>
+						<?php do_action('ylo_delete_current_post');?>
+					<?php else : ?>
+						<?php while(have_posts()) : the_post();?>
+							<div <?php post_class('ylo-post-article'); ?>>
+								<h2 class="ylo-titre-triangle">Publi&eacute; le <?php echo get_the_date();?>, par <?php the_author(); ?><span>&nbsp;</span></h2>
+								<p><?php the_content(); ?></p>		
+							</div>
+							<span><?php do_action('ylo_lien_editer_supprimer');?></span>
+							<nav class="ylo-single-page-nav">
+								<?php posts_nav_link(); ?>
+							</nav>
+							<?php comments_template();?>
+						<?php endwhile;?>
+						&nbsp;
+						
+						<?php if(current_user_can('edit_posts')) :?>
+							<hr />
+							<?php $cat = get_the_category(); ?> 
+							Publier un nouvel article dans cette la cat&eacute;gorie <?php  echo $cat[0]->cat_name; ?> 
+							<a href="<?php echo apply_filters('ylo_new_post_link', get_category_link($cat[0]->cat_ID)); ?>" >
+								<button>Nouvel Article</button>
+							</a>
+							<hr><br>
+						<?php endif;?>
 					<?php endif;?>
 				</div><!-- .ylo-col1 -->
 				<div class="ylo-col2">
@@ -36,8 +45,6 @@
 			<div class="clearfix"></div>
 		</div><!-- #ylo-content -->
 	</div><!-- #ylo-page -->		
-
-
 
 
 
