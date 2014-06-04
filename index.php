43466ec184da3c34535b475878b929b95c082cbe
audaxland <github2014@audaxland.fr>
2014-06-04 15:20:25 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
 	<div id="ylo-page">
 		<?php get_template_part('template_parts/top', 'actionbox'); ?>
 		
-		<div id="ylo-content">		
+		<div id="ylo-content" class="ylo-content">		
 			<h1 class="ylo-main-title"><?php the_title(); ?></h1>
 			<div id="ylo-main">
 				<div class="ylo-col1">
 					<?php while(have_posts()) : the_post();?>
 						<div <?php post_class(); ?>>
-							<h2 class="ylo-titre-ligne"><span><?php the_title() ;?></span></h2>
-							<p><?php the_content(); ?></p>		
+							<h2 class="ylo-titre-ligne"><span><a href="<?php the_permalink(); ?>"><?php the_title() ;?></a></span></h2>
+							<p><?php the_content(); ?></p>	
 						</div>
 					<?php endwhile;?>
 					&nbsp;
