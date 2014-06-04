0a142a8b5d6f4231dce721abc99afe91cdb21099
audaxland <github2014@audaxland.fr>
2014-06-04 15:22:27 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,7 @@
+		<footer id="ylo-page-footer">
+			<span class="ylo-copyright">&copy; Copyright <?php bloginfo('name'); ?> <?php echo date('Y');?></span>
+			<div id="ylo-footer-menu"><?php wp_nav_menu(array('theme_location' => 'ylo-footer', 'depth' => 1,'fallback_cb' => false ));?></div>
+		</footer>
 
 	</div><!-- #page-wrapper -->
 	<?php wp_footer();?>
