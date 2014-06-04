169f9ffb095a7b85aa2fcaeb59e5511d32673743
audaxland <github2014@audaxland.fr>
2014-06-04 15:26:25 +0200

    Update acces-restreint.php

--- template_parts/acces-restreint.php
+++ template_parts/acces-restreint.php
@@ -1,7 +1,7 @@
 	<div id="ylo-page">
 		<div id="ylo-action-box">
 		</div><!-- #ylo-action-box -->
-		<div id="ylo-content">
+		<div id="ylo-content" class="ylo-content">
 			<h1 class="ylo-main-title">Acc&egrave;s restreint</h1>
 			
 			<div id="ylo-main">
@@ -14,7 +14,24 @@
 						</div>
 					<div id="ylo-login-box">
 						<h3>Identifiez-vous : </h3>
-						<?php wp_login_form(); ?>
+						<?php do_action('ylo_custom_login_error'); ?>
+						<form name="loginform" id="loginform" a method="post">
+							<?php wp_nonce_field('ylo_custom_login_nonce', 'ylo_custom_login_nonce');?>
+							<p class="login-username">
+								<label for="user_login">Identifiant</label>
+								<input name="log" id="user_login" class="input" value="" size="20" type="text">
+							</p>
+							<p class="login-password">
+								<label for="user_pass">Mot de passe</label>
+								<input name="pwd" id="user_pass" class="input" value="" size="20" type="password">
+							</p>
+							
+							<p class="login-remember"><label><input name="rememberme" id="rememberme" value="forever" type="checkbox"> Se souvenir de moi</label></p>
+							<p class="login-submit">
+								<input name="wp-submit" id="wp-submit" class="button-primary" value="Se connecter" type="submit">
+							</p>
+							
+						</form>
 					</div>
 				</div><!-- .ylo-full-width -->
 			</div><!-- #ylo-main -->
