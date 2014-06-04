43440e700a1dac3cca570dbe4fb545f82efa5e81
audaxland <github2014@audaxland.fr>
2014-06-04 15:32:45 +0200

    Update yloLoginForm.php

--- lib/yloLoginForm.php
+++ lib/yloLoginForm.php
@@ -3,7 +3,7 @@ class yloLoginForm
 {
 	protected $errors = array();
 	
-	public function checkLogin(){
+	public function checkLoginLogout(){
 		if(!is_user_logged_in()){
 			if(isset($_POST['ylo_custom_login_nonce']) && wp_verify_nonce($_POST['ylo_custom_login_nonce'], 'ylo_custom_login_nonce' )){
 				$user = wp_signon();
@@ -12,17 +12,21 @@ class yloLoginForm
 					if(empty($_POST['log'])) $this->errors[] = __('Vous devez saisir un nom d&#39;utilisateur pour vous identifier.');
 					else $this->errors[] = $user->get_error_message();
 				}else{
-					// on recharge la page pour palier à un bug d'affichage que j'ai pas compris
-					wp_redirect($_SERVER['REQUEST_URI']);
+					$redirect = empty($_POST['ylo_custom_login_redirect']) ? $_SERVER['REQUEST_URI'] : esc_url($_POST['ylo_custom_login_redirect']);
+					wp_redirect($redirect);
 					exit;
 				}
 			}
+		}elseif(isset($_GET['ylo_deconnexion']) && $_GET['ylo_deconnexion'] == 'deconnecter'){
+			wp_logout();
+			wp_redirect(home_url());
+			exit;
 		}
 	}
 	
-	public function display_errors(){
+	public function display_errors($class = ''){
 		if(count($this->errors) > 0){
-			echo '<ul class="ylo-login-error">';
+			echo '<ul class="ylo-login-error '.$class.'">';
 			foreach($this->errors as $error){
 				echo '<li>'.$error.'</li>';
 			}
