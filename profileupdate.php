ce0ee49aa6597df0102a8bdcb2e046862352bc62
audaxland <github2014@audaxland.fr>
2014-06-06 15:18:30 +0200

    Update profileupdate.php

--- profileupdate.php
+++ profileupdate.php
@@ -15,6 +15,9 @@ $yloForm = apply_filters('ylo_update_form', false );
 			<h1 class="ylo-main-title">Votre Profil</h1>
 			<div id="ylo-main">
 				<div class="ylo-col1">
+				<?php if(isset($_GET['ylo_delete_user']) && $_GET['ylo_delete_user'] == 'supprimer') :?>
+					<?php do_action('ylo_delete_user');?>
+				<?php else :?>
 					<div id="ylo-loop">
 						<?php while(have_posts()) : the_post();?>
 							<div <?php post_class(); ?>>
@@ -24,7 +27,7 @@ $yloForm = apply_filters('ylo_update_form', false );
 						<?php endwhile;?>					
 					</div>
 					<div class="ylo-inscription">
-						<form id="ylo-form-inscription" method="post" action="#" enctype="multipart/form-data">
+						<form id="ylo-form-inscription" method="post"  enctype="multipart/form-data">
 							<input type="hidden" name="ylo-formulaire" value="update-profile" />
 							 <?php wp_nonce_field('ylo_update_profile','_wpnonce_ylo_update_profile'); ?>
 							<fieldset>
@@ -89,6 +92,24 @@ $yloForm = apply_filters('ylo_update_form', false );
 									<input type="text" name="ylo_first_name" id="ylo_first_name" value="<?php $yloForm->value('ylo_first_name'); ?>" placeholder="Pr&eacute;nom" />
 								</div>
 								
+							</fieldset>
+							<fieldset>
+								<div class="ylo-form-div">
+									<?php $yloForm->displayError('ylo_metier'); ?>
+									<label class="ylo-large-label" for="ylo_metier">Ton m&eacute;tier ou ta qualit&eacute; * 
+										<br /><em class="ylo-small">(d&eacute;veloppeur, d&eacute;signer, webmaster, community-manager, sys-admin, porteur de projet, client, ... )</em>
+									</label> 
+									<input type="text" name="ylo_metier" id="ylo_metier" value="<?php $yloForm->value('ylo_metier'); ?>" placeholder="M&eacute;tier ou qualit&eacute;" />
+								</div>
+								
+								<div class="ylo-form-div">
+									<label for="user_url">Ton site web</label> 
+									<input type="text" name="user_url" id="user_url" value="<?php $yloForm->value('user_url'); ?>" placeholder="Ton site web"/>
+								</div>
+								
+							</fieldset>
+							<fieldset>
+								
 								<div class="ylo-form-div">
 									<label for="ylo_ville">Ville de r&eacute;sidence</label> 
 									<input type="text" name="ylo_ville" id="ylo_ville" value="<?php $yloForm->value('ylo_ville'); ?>" placeholder="Ta ville de r&eacute;sidence" />
@@ -109,7 +130,9 @@ $yloForm = apply_filters('ylo_update_form', false );
 								
 								<div class="ylo-form-div">
 									<label class="ylo-textarea-label" for="ylo_competences">Tes domaines de comp&eacute;tence ou qualit&eacute;s</label>
-									<textarea rows="5" cols="15" name="ylo_competences" id="ylo_competences" placeholder="D&eacute;velloppeur, D&eacute;signer, Sys Amin, Community manager, Client ..."><?php $yloForm->value('ylo_competences'); ?></textarea>
+									<textarea rows="5" cols="15" name="ylo_competences" id="ylo_competences" 
+										placeholder="Html, css, php, javasript, java, photoshop, graphiste, linux, python, wordpress, synfony, jquery, gestion de projet, visionnaire, enthousiaste ..."
+									><?php $yloForm->value('ylo_competences'); ?></textarea>
 								</div>
 								
 								<div class="ylo-form-div">
@@ -123,9 +146,20 @@ $yloForm = apply_filters('ylo_update_form', false );
 								</div>
 								
 								<div class="ylo-form-div">
+									<label class="ylo-textarea-label" for="ylo_projet_realises">Projets r&eacute;alis&eacute; <em>(Seul ou avec d'autres personnes)</em></label>
+									<textarea rows="5" cols="15" name="ylo_projet_realises" id="ylo_projet_realises"><?php $yloForm->value('ylo_projet_realises'); ?></textarea>
+								</div>								
+								
+								<div class="ylo-form-div">
 									<label class="ylo-textarea-label" for="ylo_temoignage">Témoignage</label>
 									<textarea rows="5" cols="15" name="ylo_temoignage" id="ylo_temoignage" placeholder="Un court résumé..."><?php $yloForm->value('ylo_temoignage'); ?></textarea>
 								</div>
+																
+								<div class="ylo-form-div">
+									<label class="ylo-textarea-label" for="description">Coordonn&eacute;es et autres infos</label>
+									<textarea rows="5" cols="15" name="description" id="description" placeholder="N° de t&eacute;l&eacute;phone, freelance, soci&eacute;t&eacute;, association, etc ..."><?php $yloForm->value('description'); ?></textarea>
+								</div>
+								
 								
 								<div class="ylo-form-div">
 									<?php $yloForm->displayError('ylo_avatar_upload'); ?>
@@ -133,14 +167,32 @@ $yloForm = apply_filters('ylo_update_form', false );
 									<?php $yloForm->avatarValue('ylo_avatar_upload', 'ylo_avatar_existant', 'ylo_supprimer_avatar'); ?>
 									<input type="file" name="ylo_avatar_upload" id="ylo_avatar_upload" />
 								</div>
-								
+							
+							</fieldset>
+							<fieldset class="ylo-form-top-line">
+								<div class="ylo-form-div">
+									<?php $yloForm->displayError('ylo_conditions_generales'); ?>
+									<input type="checkbox" name="ylo_conditions_generales" class="ylo-checkbox" value="accepted" checked="checked" disabled="disabled"/>
+									<label class="ylo-checkbox-label" for="ylo_conditions_generales">
+										<?php $yloForm->conditionsGenerales();?>
+									</label>
+								</div>
 								<div class="ylo-form-div form-submit ">
 									<input class="ylo-bouton-1" type="submit" value="Envoyer" />
 								</div>
 								
 							</fieldset>
 						</form>
+						<div class="ylo-lien-suppression-user">
+							<p>
+								<a href="<?php echo add_query_arg('ylo_delete_user', 'supprimer', $_SERVER['REQUEST_URI']);?>">
+									Supprimer votre compte
+								</a>
+								- Attention : la suppression de votre compte de membre est irr&eacute;versible 
+							</p>
+						</div>
 					</div>
+					<?php endif;?>
 				</div><!-- .ylo-col1 -->
 				<div class="ylo-col2">
 					<?php do_action('ylo_default_sidebar');  ?>
