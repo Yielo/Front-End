c7ad8459776b9d68f872690b69ee743ad9e830a2
audaxland <github2014@audaxland.fr>
2014-06-06 15:26:17 +0200

    Update yloUserFields.php

--- lib/yloUserFields.php
+++ lib/yloUserFields.php
@@ -2,23 +2,30 @@
 class yloUserFields
 {
 	protected static $yloChamps = array(
-			'ylo_eglise' => 'Ton &eacute;glise',
+			
 			'ylo_ville' => 'Ville de r&eacute;sidence',
 			'ylo_pays' => 'Pays de r&eacute;sidence',
+			'ylo_eglise' => 'Ton &eacute;glise',
+			'ylo_metier'	=> 'Ton m&eacute;tier ou ta qualit&eacute;',
 			'ylo_competences' => 'Tes domaines de comp&eacute;tences',
 			'ylo_formation' => 'Ta formation',
 			'ylo_experiences_pro' => 'Tes exp&eacute;riences professionnelles',
 			'ylo_temoignage'	=> 'Ton t&eacute;moignage',
-			//'ylo_avatar_upload' => 'Ton avatar',
+			'ylo_projet_realises' => 'Les projets que tu as r&eacute;alis&eacute;',
+			
+			
+			//'ylo_avatar_upload' => 'Ton avatar', // l'avatar est une metadonnée et non un champs de contact
 		);
 	
 	protected static $champsNatifs = array(
 			'user_login'    =>  'Ton Login',
 			'user_email'	=>	'Ton Email',
+			'user_url'		=>	'Ton Site Web',
 			// 'user_pass'		=>	'', le mot de passe ne doit être utilisié qu'intentionnellement, d'ou l'intérêt de le supprimer de cette liste
 			'first_name'	=>	'Ton Pr&eacute;nom',
 			'last_name'		=>	'Ton nom',
 			'nickname'		=>	'Ton pseudo',
+			'description'	=>	'Coordonn&eacute;es et autres infos',
 		);
 	
 	public static function fieldList(){
