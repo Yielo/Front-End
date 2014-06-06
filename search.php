4539f314d89927094b8a1b0dc7864b12454f3464
audaxland <github2014@audaxland.fr>
2014-06-06 15:17:06 +0200

    Create search.php

---
+++ search.php
@@ -0,0 +1,13 @@
+<?php do_action('ylo_search_hooks');?>
+<?php get_header(); ?>
+
+	<div id="ylo-page">
+		<div id="ylo-content" class="ylo-content">		
+			<?php get_template_part('template_parts/recherche');?>
+			<div class="clearfix"></div>
+		</div><!-- #ylo-content -->
+	</div><!-- #ylo-page -->		
+
+
+<div style="padding: 10px;" >
+<?php get_footer(); ?>
