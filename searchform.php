7c95348f279b3b2ac153495da09fdf1d53e3c019
audaxland <github2014@audaxland.fr>
2014-06-07 22:31:51 +0200

    Update searchform.php

--- searchform.php
+++ searchform.php
@@ -1,8 +1,8 @@
 	<div class="ylo-search-container">
 		<form id="search" method="get" action="<?php echo home_url( '/' ); ?>" >
 			<fieldset>
-				<input type="text" name="s" value="" placeholder="Un membre, un métier , un lieu,..." />
-				<input type="submit" value="Rechercher" class="ylo-search-button" />
+				<input type="text" name="s" value="" placeholder="Un membre, un métier , un lieu,..." /><input type="submit" value="Rechercher" class="ylo-search-button" />
+				
 			</fieldset>
 		</form>
 	</div>
